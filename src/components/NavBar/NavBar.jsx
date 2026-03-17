8d13472952ac895e4c495298d0cd3823ed954582
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-17 23:54:59 +0530

    navbar created

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,60 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 
 import Link from "./Link";
+import { Menu, X } from "lucide-react";
 
 const navItems = [
   {
     id: 1,
     name: "Home",
-    path: "/"
+    path: "/",
   },
   {
     id: 2,
     name: "About",
-    path: "/about"
+    path: "/about",
   },
   {
     id: 3,
     name: "Services",
-    path: "/services"
+    path: "/services",
   },
   {
     id: 4,
     name: "Contact",
-    path: "/contact"
+    path: "/contact",
   },
   {
     id: 5,
     name: "Dashboard",
-    path: "/dashboard"
-  }
+    path: "/dashboard",
+  },
 ];
 
-
-
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setOpen(!open);
+  };
+
   return (
     <div>
+      <nav className="flex justify-between p-6 items-center">
+        <span onClick={handleMenuToggle} className="flex gap-4 items-center">
+          
+          {open ? 
+           <X></X>  : 
+         <Menu></Menu> }
 
 
+          <h1 className="text-2xl">Pricing </h1>
+        </span>
 
-<ul className="flex gap-5">
-    {
-        navItems.map(route => <Link key={route.id} route={route}></Link>)
-    }
-</ul>
+        <ul className="flex gap-5">
+          {navItems.map((route) => (
+            <Link key={route.id} route={route}></Link>
+          ))}
+        </ul>
 
+        <button className="btn btn-outline btn-primary">Primary</button>
+      </nav>
 
-
-
-{/* 
+      {/* 
 <ul className="flex gap-5">
 {
     navItems.map(route =><li><a href={route.path}>{route.name}</a></li> )
 }
 </ul> */}
-
 
       {/* <ul>
     <li><a href="/home">Home</a></li>
